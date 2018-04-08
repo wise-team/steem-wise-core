2e3dd7b1c560c6e5981d6d0afc3ff71f57265e4a
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-04-08 23:21:51 +0200

    Add flags to schema converter

--- schema/convert-schema-ts-to-json.sh
+++ schema/convert-schema-ts-to-json.sh
@@ -2,4 +2,8 @@
 
 npm install tslint typescript-json-schema
 
-./node_modules/typescript-json-schema/bin/typescript-json-schema --out "smartvotes.schema.json" --required "ts/*.schema.ts" "*"
+echo "Converting ts to json-schema..."
+
+./node_modules/typescript-json-schema/bin/typescript-json-schema --strictNullChecks --required --out "smartvotes.schema.json" "ts/*.schema.ts" "smartvotes_transaction"
+
+echo "Done."
