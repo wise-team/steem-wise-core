0f8409ad200480b9996eecc94df74a11bcf26832
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-04-16 18:08:32 +0200

    Add send-vote example (node)

--- examples/node/send-vote.js
+++ examples/node/send-vote.js
@@ -1,35 +1,46 @@
-import {smartvotes_vote_weight} from "../../schema/votes.schema";
-
+var fs = require("fs");
 var smartvotesLib = require("../../dist/lib/steem-smartvotes.js");
-var fs = require('fs');
-
 /**
  * Note that smartvotesLib is a module namespace. To access main class use *steemsmartvotes.SteemSmartvotes*.
  * #validateJson() is a static method of the class.
  */
 
-var credentials = {username: "", keyWif: ""};
+loadCredentials(function(credentials) {
+    sendVote(credentials)
+});
 
-/* credentials file should be a JSON file with the same structure as the credentials var. */
-if(process.argv.length > 2) {
-    var credentialsFilePath = process.argv[2];
-    fs.readFile(credentialsFilePath, function (error, content) {
-        if (error) console.err("Could not load credentials file: "+error.message);
-        else credentials = JSON.parse(content);
-    });
+function loadCredentials(callback) {
+    var credentials = {username: "", keyWif: ""}
+    /* credentials file should be a JSON file with the same structure as the credentials var. */
+    if (process.argv.length > 2) {
+        var credentialsFilePath = process.argv[2];
+            fs.readFile(credentialsFilePath, function (error, content) {
+                if (error) console.err("Could not load credentials file: " + error.message);
+                else {
+                    credentials = JSON.parse(content);
+                    console.log("Using credentials from " + credentialsFilePath);
+                }
+                callback(credentials);
+        });
+    }
+    else callback(credentials);
 }
 
-var vote = {
-    author: "steemit",
-    permalink: "firstpost",
-    delegator: "steemprojects1",
-    weight: 10000,
-    type: "vote"
-};
+function sendVote(credentials) {
+    var vote = {
+        author: "steemit",
+        permalink: "firstpost",
+        delegator: "steemprojects1",
+        weight: 10000,
+        type: "vote"
+    };
 
-var smartvotes = new smartvotesLib.SteemSmartvotes(credentials.username, credentials.keyWif);
-smartvotes.sendVote(vote, function(error) {
-    if(!error) {
-
-    }
-});
+    var smartvotes = new smartvotesLib.SteemSmartvotes(credentials.username, credentials.keyWif);
+    smartvotes.sendVoteOrder(vote, function(error) {
+        if(error) {
+            console.error(error);
+            return;
+        }
+        console.log("Vote sent. You can see it on: https://steemd.com/@"+credentials.username);
+    });
+}
