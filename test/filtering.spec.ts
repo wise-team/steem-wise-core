288d3b800706519b67df653a819d38750f5629cf
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-04-20 21:42:00 +0200

    Update filtering.spec.ts test for new filtering

--- test/filtering.spec.ts
+++ test/filtering.spec.ts
@@ -1,19 +1,18 @@
 import { expect } from "chai";
 import "mocha";
 
-import { Promise } from "bluebird";
-
 import * as filter from "../src/blockchain-filter";
 import { RawOperation, CustomJsonOperation } from "../src/blockchain-operations-types";
+import { smartvotes_operation } from "../src/schema/smartvotes.schema";
 
 
 describe("blockchain-filter", () => {
     describe("getSmartvotesOperationsOfUser", () => {
-        let guest123Ops: CustomJsonOperation[];
+        let guest123Ops: smartvotes_operation[];
 
         before(function(done) {
             this.timeout(10000);
-            filter.getSmartvotesOperationsOfUser("guest123", function(error: Error, result: CustomJsonOperation[]) {
+            filter.getSmartvotesOperationsOfUser("guest123", function(error: Error, result: smartvotes_operation []) {
                 if (error) throw error;
 
                 guest123Ops = result;
