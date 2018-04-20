a55d9fbccde3065c711e31944c9aa4b365cdea67
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-04-20 21:41:09 +0200

    Add more specific blockchain filtering

--- src/blockchain-filter.ts
+++ src/blockchain-filter.ts
@@ -1,20 +1,54 @@
+import { smartvotes_operation } from "./schema/smartvotes.schema";
+
 const steem = require("steem");
 
 import { RawOperation, CustomJsonOperation } from "./blockchain-operations-types";
 
-export function getSmartvotesOperationsOfUser(username: string, callback: (error: Error, result: CustomJsonOperation []) => void): void {
-    filterHistoryRange(username, -1, 1000, [], callback);
+/**
+ * Searches blockchain for smartvotes operations of an user.
+ * @param {string} username — voter or delegator username.
+ * @param {string[]} commands — list of commands to search for. If you leave it empty — all valid smartvote commands
+ *     will be included in the results.
+ * @param {(error: Error, result: smartvotes_operation []) => void} callback
+ */
+export function getOperations(username: string, commands: string [], callback: (error: Error, result: smartvotes_operation []) => void): void {
+    filterHistoryRange(username, -1, [], function(op: smartvotes_operation): boolean {
+        if (commands.length == 0) return true;
+        else if (commands.indexOf(op.name) !== -1) {
+            return true;
+        }
+        else return false;
+    }, callback);
 }
 
-function filterHistoryRange(username: string, from: number, limit: number, recentOps: CustomJsonOperation [], callback: (error: Error, result: CustomJsonOperation []) => void) {
-    steem.api.getAccountHistory(username, from, limit, function(error: Error, result: any) {
+/**
+ * Returns all smartvote operations of a user
+ * @param {string} username
+ * @param {(error: Error, result: smartvotes_operation[]) => void} callback
+ */
+export function getSmartvotesOperationsOfUser(username: string, callback: (error: Error, result: smartvotes_operation []) => void): void {
+    filterHistoryRange(username, -1, [], undefined, callback);
+}
+
+/**
+ * Recursive function to iterate all blockchain operations of a user.
+ * @param {string} username
+ * @param {number} from — absolute number of operation (or -1 if we want the most recent one). We start
+ * with -1 and then pass absolute numbers of operations minus one until less than 1000 operations is returned.
+ * @param {smartvotes_operation[]} recentOps — array of operations loaded so far sorted from the newest to the oldest.
+ * @param {((op: smartvotes_operation) => boolean) | undefined} filter — custom filter or undefined.
+ * @param {(error: Error, result: smartvotes_operation[]) => void} callback
+ */
+function filterHistoryRange(username: string, from: number, recentOps: smartvotes_operation [],
+                            filter: ((op: smartvotes_operation) => boolean) | undefined, callback: (error: Error, result: smartvotes_operation []) => void) {
+    steem.api.getAccountHistory(username, from, 1000, function(error: Error, result: any) {
         if (error) callback(error, []);
         else {
             if (result.length == 0) {
                 callback(error, recentOps);
             }
             else {
-                const resultFiltered: CustomJsonOperation [] = filterOps(result);
+                const resultFiltered: smartvotes_operation [] = filterAndTransformSmartvoteOps(result, filter);
                 recentOps = resultFiltered.concat(recentOps);
 
                 if (result.length < 1000) { // all operations were loaded
@@ -22,21 +56,31 @@ function filterHistoryRange(username: string, from: number, limit: number, recen
                 }
                 else { // if length == 1000 -> there are more ops to load
                     const from = result[0][0] - 1; // absolute number of oldest loaded operation, minus one
-                    filterHistoryRange(username, from, 1000, recentOps, callback);
+                    filterHistoryRange(username, from, recentOps, filter, callback);
                 }
             }
         }
     });
 }
 
-
-function filterOps(rawOps: RawOperation []): CustomJsonOperation [] {
-    const out: CustomJsonOperation [] = [];
+/**
+ * Filters and transforms smartvotes operations out of all user's operations.
+ * @param {RawOperation[]} rawOps — list of all user's operations
+ * @param {(op: smartvotes_operation) => boolean} filter — custom filter for operations
+ * @returns {CustomJsonOperation[]} — list of user's smartvotes operations
+ */
+function filterAndTransformSmartvoteOps(rawOps: RawOperation [], filter: ((op: smartvotes_operation) => boolean) | undefined): smartvotes_operation [] {
+    const out: smartvotes_operation [] = [];
 
     for (const i in rawOps) {
         const rawOp: RawOperation = rawOps[i];
         if (rawOp[1].op[0] == "custom_json" && rawOp[1].op[1].id == "smartvote") {
-            out.push(rawOp[1].op[1]);
+                const jsonStr: string = rawOp[1].op[1].json;
+                const op: smartvotes_operation = JSON.parse(jsonStr) as smartvotes_operation;
+
+            if (typeof filter == "undefined" || filter == undefined || filter(op)) {
+                out.push(op);
+            }
         }
     }
 
--- src/blockchain-operations-types.ts
+++ src/blockchain-operations-types.ts
@@ -1,4 +1,16 @@
-export type RawOperation = [number, {block: number, op: [string, CustomJsonOperation], op_in_trx: number, timestamp: string, trx_id: string, trx_in_block: number, virtual_op: number}];
+export type RawOperation =
+    [
+        number,
+        {
+            block: number,
+            op: [string, CustomJsonOperation],
+            op_in_trx: number,
+            timestamp: string,
+            trx_id: string,
+            trx_in_block: number,
+            virtual_op: number
+        }
+    ];
 
 export type CustomJsonOperation = {
     id: string;
