aff866067236057fa58d08550194a37ea327129b
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-05-05 23:17:11 +0200

    Add confirm_vote command to schema

--- src/schema/smartvotes.schema.ts
+++ src/schema/smartvotes.schema.ts
@@ -13,7 +13,7 @@ import { smartvotes_voteorder } from "./votes.schema";
  * General type for every smartvotes operation. If an custom_json operation on steem blockchain has
  * an id=smartvote parameter it is going to be parsed as a smartvotes operation using the following schema:
  */
-export type smartvotes_operation = smartvotes_command_set_rules | smartvotes_command_send_voteorder;
+export type smartvotes_operation = smartvotes_command_set_rules | smartvotes_command_send_voteorder | smartvotes_command_confirm_vote;
 
 // TODO separate set_rules for every voter (8kb limit https://github.com/steemit/steem/commit/81f4c6373f1ddeb8c435b5c91fa67bcd361e29a3#diff-04f6af9cdbf180a31ccdbf6335b8f240R1517)
 /**
@@ -40,6 +40,18 @@ export interface smartvotes_command_send_voteorder {
      * A voteorder to be sent.
      */
     voteorder: smartvotes_voteorder;
+}
+
+/**
+ * This command confirms that a vote order has been accomplished.
+ */
+export interface smartvotes_command_confirm_vote {
+    name: "confirm_vote";
+
+    /**
+     * Transaction id of a voteorder.
+     */
+    transaction_id: string;
 }
 
 
