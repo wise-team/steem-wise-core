18a679b605632ecf6f5121e9de8de69b8d2a5179
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-05-08 19:37:56 +0200

    Add sorting and duplicate removal to synchronizer

--- src/blockchain/Synchronizer.ts
+++ src/blockchain/Synchronizer.ts
@@ -6,32 +6,35 @@ import { smartvotes_operation, smartvotes_command_set_rules, smartvotes_voteorde
 import { RawOperation } from "../blockchain/blockchain-operations-types";
 import { _objectAssign } from "../util/util";
 import { SteemOperationNumber } from "../blockchain/SteemOperationNumber";
+import { RulesValidator } from "../validation/RulesValidator";
 
-interface BeforeSyncData1 {
-    rulesets: {
-        tn: SteemOperationNumber,
-        ruleset: smartvotes_ruleset,
-        validityUntil: SteemOperationNumber
-    } [];
-    confirmedVotes: {
-        tn: SteemOperationNumber,
-        voteorderTransactionId: string
-    } [];
+interface VoteConfirmedAtMoment {
+    opNum: SteemOperationNumber;
+    voteorderTransactionId: string;
 }
 
-/*interface BeforeSyncData2 extends BeforeSyncData1 {
-    oldestTn: TransactionNumber;
-}*/
+interface RulesetAtMoment {
+    opNum: SteemOperationNumber;
+    ruleset: smartvotes_ruleset;
+    validityUntil: SteemOperationNumber;
+}
+
+interface VoteorderAtMoment {
+    transactionId: string;
+    opNum: SteemOperationNumber;
+    voter: string;
+    voteorder: smartvotes_voteorder;
+}
+
+/*interface BeforeSyncData1 {
+    rulesets: RulesetAtMoment [];
+    confirmedVotes: VoteConfirmedAtMoment [];
+}
 
 interface BeforeSyncData2 extends BeforeSyncData1 {
-    voteorders: {
-        tn: SteemOperationNumber,
-        voter: string,
-        voteorder: smartvotes_voteorder
-    } [];
-}
+    voteorders: VoteorderAtMoment [];
+}*/
 
-// TODO move to /blockchain
 export class Synchronizer {
     private steem: any;
     private username: string;
@@ -55,15 +58,18 @@ export class Synchronizer {
         .then((input: any) => { this.proggressCallback("Loading new voteorders from voters you delegated to", 0.2); return input; })
         .then(this.loadVoteorders)
         .then((input: any) => { this.proggressCallback("Loaded all voteorders, validating...", 0.6); return input; })
-        .then(this.validateVoteorders)
+        .then(this.removeAlreadyConfirmedVotes)
+        .then(this.sortVoteordersFromOldestToNewest)
+        .then(this.removeDuplicateVoteorders)
+        .then(this.validateAndSend)
         .then((input: any) => { callback(undefined); })
         .catch((error: Error) => callback(error));
     }
 
-    private loadUnsynchronizedOperations = (): Promise<BeforeSyncData1> => {
+    private loadUnsynchronizedOperations = (): Promise<{rulesets: RulesetAtMoment [], confirmedVotes: VoteConfirmedAtMoment []}> => {
         return new Promise((resolve, reject) => {
-            let rulesets: {tn: SteemOperationNumber, ruleset: smartvotes_ruleset, validityUntil: SteemOperationNumber} [] = [];
-            const confirmedVotes: {tn: SteemOperationNumber, voteorderTransactionId: string} [] = [];
+            let rulesets: RulesetAtMoment [] = [];
+            const confirmedVotes: VoteConfirmedAtMoment [] = [];
 
             let foundVoteConfirmation = false;
             new AccountHistorySupplier(this.steem, this.username)
@@ -74,7 +80,7 @@ export class Synchronizer {
                 .chain(new SimpleTaker((item: {rawOp: RawOperation, op: smartvotes_operation}): boolean => {
                     if (item.op.name === "confirm_vote") {
                         confirmedVotes.push({
-                            tn: SteemOperationNumber.fromOperation(item.rawOp),
+                            opNum: SteemOperationNumber.fromOperation(item.rawOp),
                             voteorderTransactionId: item.op.transaction_id
                         });
                         foundVoteConfirmation = true;
@@ -83,7 +89,7 @@ export class Synchronizer {
                         for (let i = 0; i < item.op.rulesets.length; i++) {
                             const ruleset = item.op.rulesets[i];
                             rulesets.push({
-                                tn: SteemOperationNumber.fromOperation(item.rawOp),
+                                opNum: SteemOperationNumber.fromOperation(item.rawOp),
                                 ruleset: ruleset,
                                 validityUntil: new SteemOperationNumber(Infinity, Infinity, Infinity)
                             });
@@ -105,17 +111,17 @@ export class Synchronizer {
         });
     }
 
-    private calculateRulesetValidityInterval = (rulesets: {tn: SteemOperationNumber, ruleset: smartvotes_ruleset, validityUntil: SteemOperationNumber} []): {tn: SteemOperationNumber, ruleset: smartvotes_ruleset, validityUntil: SteemOperationNumber} [] => {
+    private calculateRulesetValidityInterval = (rulesets: RulesetAtMoment []) => {
         rulesets.sort((a, b) => {
-            if (a.tn.isLesserThan(b.tn))
+            if (a.opNum.isLesserThan(b.opNum))
                 return -1;
-            if (a.tn.isGreaterThan(b.tn))
+            if (a.opNum.isGreaterThan(b.opNum))
                 return 1;
             return 0;
         });
         for (let i = 0; i < rulesets.length; i++) {
             if (i + 1 < rulesets.length)
-                rulesets[i].validityUntil = rulesets[i + 1].tn;
+                rulesets[i].validityUntil = rulesets[i + 1].opNum;
             else
                 rulesets[i].validityUntil = new SteemOperationNumber(Infinity, Infinity, Infinity);
         }
@@ -144,7 +150,7 @@ export class Synchronizer {
         });
     }*/
 
-    private loadVoteorders = (input: BeforeSyncData1): Promise<BeforeSyncData2> => {
+    private loadVoteorders = (input: {rulesets: RulesetAtMoment [], confirmedVotes: VoteConfirmedAtMoment []}): Promise<{rulesets: RulesetAtMoment [], confirmedVotes: VoteConfirmedAtMoment [], voteorders: VoteorderAtMoment []}> => {
         const voters: string [] = [];
         const votersLookupSince: SteemOperationNumber [] = [];
 
@@ -152,24 +158,24 @@ export class Synchronizer {
             const ruleset = input.rulesets[i];
             if (voters.indexOf(ruleset.ruleset.voter) !== -1) {
                 const index = voters.indexOf(ruleset.ruleset.voter);
-                if (ruleset.tn.isLesserThan(votersLookupSince[index])) votersLookupSince[index] = ruleset.tn;
+                if (ruleset.opNum.isLesserThan(votersLookupSince[index])) votersLookupSince[index] = ruleset.opNum;
             }
             else {
                 voters.push(ruleset.ruleset.voter);
-                votersLookupSince.push(ruleset.tn);
+                votersLookupSince.push(ruleset.opNum);
             }
         }
 
-        const promises: Promise<{voter: string, voteorder: smartvotes_voteorder, tn: SteemOperationNumber}[]> [] = [];
+        const promises: Promise<VoteorderAtMoment[]> [] = [];
         for (let i = 0; i < voters.length; i++) {
             promises.push(this.loadUserVoteorders(voters[i], votersLookupSince[i]));
         }
 
         return Promise.all(promises).then(
-            (value: {}[]): BeforeSyncData2 => {
-                const voteorders: {tn: SteemOperationNumber, voter: string, voteorder: smartvotes_voteorder} [] = [];
+            (value: {}[]): {rulesets: RulesetAtMoment [], confirmedVotes: VoteConfirmedAtMoment [], voteorders: VoteorderAtMoment []} => {
+                const voteorders: VoteorderAtMoment [] = [];
                 for (let i = 0; i < value.length; i++) {
-                    const voteorder = value[i] as {tn: SteemOperationNumber, voter: string, voteorder: smartvotes_voteorder};
+                    const voteorder = value[i] as VoteorderAtMoment;
                     voteorders.push(voteorder);
                 }
                 return {confirmedVotes: input.confirmedVotes, rulesets: input.rulesets, voteorders: voteorders};
@@ -177,10 +183,10 @@ export class Synchronizer {
         );
     }
 
-    private loadUserVoteorders = (voter: string, lookupSince: SteemOperationNumber): Promise<{voter: string, voteorder: smartvotes_voteorder, tn: SteemOperationNumber}[]> => {
+    private loadUserVoteorders = (voter: string, lookupSince: SteemOperationNumber): Promise<VoteorderAtMoment[]> => {
         this.proggressCallback("Loading voteorders from " + voter + "...", 0.4);
 
-        const voteorders: {voter: string, voteorder: smartvotes_voteorder, tn: SteemOperationNumber} [] = [];
+        const voteorders: VoteorderAtMoment [] = [];
         return new Promise((resolve, reject) => {
             new AccountHistorySupplier(this.steem, voter)
             .branch((historySupplier) => {
@@ -190,7 +196,12 @@ export class Synchronizer {
                 .chain(new BiTransformer())
                 .chain(new SimpleTaker((item: {rawOp: RawOperation, op: smartvotes_operation}): boolean => {
                     if (item.op.name === "send_voteorder") {
-                        voteorders.push({voter: voter, voteorder: item.op.voteorder, tn: SteemOperationNumber.fromOperation(item.rawOp)});
+                        voteorders.push({
+                            voter: voter,
+                            voteorder: item.op.voteorder,
+                            opNum: SteemOperationNumber.fromOperation(item.rawOp),
+                            transactionId: item.rawOp[1].trx_id
+                        });
                     }
 
                     return true;
@@ -207,9 +218,74 @@ export class Synchronizer {
         });
     }
 
-    private validateVoteorders = (input: BeforeSyncData2): Promise<void> => {
+    private removeAlreadyConfirmedVotes = (input: {rulesets: RulesetAtMoment [], confirmedVotes: VoteConfirmedAtMoment [],
+                voteorders: VoteorderAtMoment []}): Promise<{rulesets: RulesetAtMoment [],
+                confirmedVotes: VoteConfirmedAtMoment [], voteorders: VoteorderAtMoment []}> => {
+        return new Promise((resolve, reject) => {
+            const unsyncedVoteorders: VoteorderAtMoment [] = [];
+
+            for (let i = 0; i < input.voteorders.length; i++) {
+                const voteorder = input.voteorders[i];
+                let unsynced = true;
+                for (let j = 0; i < input.confirmedVotes.length; j++) {
+                    const confirmedVote = input.confirmedVotes[j];
+                    if (voteorder.transactionId === confirmedVote.voteorderTransactionId) {
+                        unsynced = false;
+                        break;
+                    }
+                }
+                if (unsynced) unsyncedVoteorders.push(voteorder);
+            }
+
+            resolve({voteorders: unsyncedVoteorders, rulesets: input.rulesets, confirmedVotes: input.confirmedVotes});
+        });
+    }
+
+    private sortVoteordersFromOldestToNewest = (input: {rulesets: RulesetAtMoment [], confirmedVotes: VoteConfirmedAtMoment [],
+            voteorders: VoteorderAtMoment []}): Promise<{rulesets: RulesetAtMoment [], confirmedVotes: VoteConfirmedAtMoment [],
+            voteorders: VoteorderAtMoment []}> => {
+        return new Promise((resolve, reject) => {
+            const voteorders = input.voteorders; // there is no need to copy them (just sort)
+            voteorders.sort((a, b): number => {
+                return a.opNum.isGreaterThan(b.opNum) ? -1 : a.opNum.isLesserThan(b.opNum) ? 1 : 0;
+            });
+
+            resolve({voteorders: voteorders, rulesets: input.rulesets, confirmedVotes: input.confirmedVotes});
+        });
+    }
+
+    private removeDuplicateVoteorders = (input: {rulesets: RulesetAtMoment [], confirmedVotes: VoteConfirmedAtMoment [],
+        voteorders: VoteorderAtMoment []}): Promise<{rulesets: RulesetAtMoment [], confirmedVotes: VoteConfirmedAtMoment [],
+        voteorders: VoteorderAtMoment []}> => {
+        return new Promise((resolve, reject) => {
+            const cleanedVoteorders: VoteorderAtMoment [] = []; // there is no need to copy them (just sort)
+            for (let i = 0; i < input.voteorders.length; i++) {
+                const voteorderCandidate = input.voteorders[i];
+                let duplicate = false;
+                for (let j = 0; j < cleanedVoteorders.length; j++) {
+                    const potentiallyDuplicated = cleanedVoteorders[j];
+                    if (potentiallyDuplicated.voteorder.author === voteorderCandidate.voteorder.author
+                        && potentiallyDuplicated.voteorder.permlink === voteorderCandidate.voteorder.permlink) {
+                            duplicate = true;
+                            cleanedVoteorders.splice(j, 1); // ensure only the newest voteorder for the post is used (that is why we remove older one).
+                            cleanedVoteorders.push(voteorderCandidate);
+                        }
+                }
+                if (!duplicate) cleanedVoteorders.push(voteorderCandidate);
+            }
+
+            resolve({voteorders: cleanedVoteorders, rulesets: input.rulesets, confirmedVotes: input.confirmedVotes});
+        });
+    }
+
+    private validateAndSend = (input: {rulesets: RulesetAtMoment [], confirmedVotes: VoteConfirmedAtMoment [],
+            voteorders: VoteorderAtMoment []}): Promise<void> => {
         /* tslint:disable no-null-keyword  */
         return new Promise((resolve, reject) => {
+            for (let i = 0; i < input.voteorders.length; i++) {
+                const voteorder = input.voteorders[i];
+            }
+
             console.log("Loaded: ");
             console.log(JSON.stringify(input, null, 2));
 
