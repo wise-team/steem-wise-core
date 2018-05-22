0cf61d415dd50fafefdcb6c2189b6d5f6bab9157
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-05-22 19:54:36 +0200

    New protocol ideas

--- src/schema/smartvotes.schema.ts
+++ src/schema/smartvotes.schema.ts
@@ -9,6 +9,14 @@
 import { smartvotes_ruleset } from "./rules.schema";
 import { smartvotes_voteorder } from "./votes.schema";
 
+// TODO idea: steem custom_json.id should include delegator username: less overhead in filtration (no need to parse every json)
+// Max length of custom_json.id is 32 characters: https://github.com/steemit/steem/blob/bb8ca476f33c08434274f2d76333fcb74cfa58b8/libraries/protocol/steem_operations.cpp#L294
+// but max length of username: https://github.com/steemit/steem/blob/62b48877d9f731c3fe00ef818e3324a0a3de3e63/libraries/protocol/include/steem/protocol/config.hpp#L215
+// authority.cpp#is_valid_account_name: https://github.com/steemit/steem/blob/1cfdf8101ec415156b155c9ec90b0a4d439a039f/libraries/protocol/authority.cpp#L46
+// proposed custom_json.id format: "[wise=4chars][protocolversion-1char]:[rules_for|voteorder|v_confirm=9characters]:[delegator=3-16characters]".
+// that is 4+1+1+9+1+(3-16) = 19-32
+// longest example: "wise1:v_confirm:0123456789abcdef". length=32 characters.
+
 /**
  * General type for every smartvotes operation. If an custom_json operation on steem blockchain has
  * an id=smartvote parameter it is going to be parsed as a smartvotes operation using the following schema:
