4bebfdb056cc17dde8569c941e937318ba9e5de2
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-05-26 20:23:19 +0200

    Add type guards for Commands

--- src/protocol/SendVoteorder.ts
+++ src/protocol/SendVoteorder.ts
@@ -1,6 +1,18 @@
+import { SetRules } from "./SetRules";
+
 export interface SendVoteorder {
     rulesetName: string;
     permlink: string;
     author: string;
     weight: number;
+}
+
+/**
+ * This is an TS 1.6+ TypeGuard as described here: https://www.typescriptlang.org/docs/handbook/advanced-types.html
+ */
+export function isSendVoteorder(cmd: SetRules | SendVoteorder): cmd is SendVoteorder {
+    return (<SendVoteorder>cmd).rulesetName !== undefined
+        && (<SendVoteorder>cmd).permlink !== undefined
+        && (<SendVoteorder>cmd).author !== undefined
+        && (<SendVoteorder>cmd).weight !== undefined;
 }
--- src/protocol/SetRules.ts
+++ src/protocol/SetRules.ts
@@ -1,8 +1,16 @@
 import { Rule } from "../rules/Rule";
+import { SendVoteorder } from "./SendVoteorder";
 
 export interface SetRules {
     rulesets: {
         name: string;
         rules: Rule [];
     } [];
+}
+
+/**
+ * This is an TS 1.6+ TypeGuard as described here: https://www.typescriptlang.org/docs/handbook/advanced-types.html
+ */
+export function isSetRules(cmd: SetRules | SendVoteorder): cmd is SetRules {
+    return (<SetRules>cmd).rulesets !== undefined;
 }
