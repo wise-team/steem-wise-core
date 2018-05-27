2261c293b15dd39acbfde87194d3bca626e2aaf2
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-05-27 22:45:30 +0200

    Implement promisification of getPost in authors and tags rule

--- src/rules/AuthorsRule.ts
+++ src/rules/AuthorsRule.ts
@@ -5,6 +5,7 @@ import { SmartvotesOperation } from "../protocol/SmartvotesOperation";
 import { ValidationError } from "../validation/ValidationError";
 import { ValidationContext } from "../validation/ValidationContext";
 import { SendVoteorder } from "../protocol/SendVoteorder";
+import { SteemPost } from "../blockchain/SteemPost";
 
 export class AuthorsRule extends Rule {
     public authors: string [];
@@ -22,16 +23,18 @@ export class AuthorsRule extends Rule {
     }
 
     public validate (voteorder: SendVoteorder, context: ValidationContext): Promise<true> {
-        return new Promise((resolve, reject) => {
-            const authorIsOnList: boolean = (this.authors.indexOf(context.getPost().author) !== -1);
-            if (this.mode == AuthorsRule.Mode.ALLOW) {
-                if (authorIsOnList) resolve(true);
-                else reject(new ValidationError("Author of the post is not on the allow list."));
-            }
-            else {
-                if (authorIsOnList) reject(new ValidationError("Author of the post is on the deny list."));
-                else resolve(true);
-            }
+        return context.getPost().then((post: SteemPost): Promise<true> => {
+            return new Promise((resolve, reject) => {
+                const authorIsOnList: boolean = (this.authors.indexOf(post.author) !== -1);
+                if (this.mode == AuthorsRule.Mode.ALLOW) {
+                    if (authorIsOnList) resolve(true);
+                    else reject(new ValidationError("Author of the post is not on the allow list."));
+                }
+                else {
+                    if (authorIsOnList) reject(new ValidationError("Author of the post is on the deny list."));
+                    else resolve(true);
+                }
+            });
         });
     }
 }
--- src/rules/TagsRule.ts
+++ src/rules/TagsRule.ts
@@ -22,44 +22,46 @@ export class TagsRule extends Rule {
     }
 
     public validate (voteorder: SendVoteorder, context: ValidationContext): Promise<true> {
-        return new Promise((resolve, reject) => {
-            const postMetadata: SteemPost.JSONMetadata = JSON.parse(context.getPost().json_metadata) as SteemPost.JSONMetadata;
+        return context.getPost().then((post: SteemPost): Promise<true> => {
+            return new Promise((resolve, reject) => {
+                const postMetadata: SteemPost.JSONMetadata = JSON.parse(post.json_metadata) as SteemPost.JSONMetadata;
 
-            if (this.mode === TagsRule.Mode.ALLOW) { // allow mode (every post tag must be within this list)
-                for (let i = 0; i < postMetadata.tags.length; i++) {
-                    const tag = postMetadata.tags[i];
-                    if (this.tags.indexOf(tag) === -1)
-                            reject(new ValidationError("Tag " + tag + " is not on the allowed tags list [" + this.tags.join() + "]."));
-                }
-                resolve(true);
-            }
-            else if (this.mode === TagsRule.Mode.DENY) { // deny mode (none of post tags can be on this list)
-                for (let i = 0; i < postMetadata.tags.length; i++) {
-                    const tag = postMetadata.tags[i];
-                    if (this.tags.indexOf(tag) !== -1)
-                            reject(new ValidationError("Tag " + tag + " is on the denied tags list [" + this.tags.join() + "]."));
-                }
-                resolve(true);
-            }
-            else if (this.mode === TagsRule.Mode.REQUIRE) { // the post should have all of the specified tags
-                for (let i = 0; i < this.tags.length; i++) {
-                    const tag = this.tags[i];
-                    if (postMetadata.tags.indexOf(tag) === -1)
-                        reject(new ValidationError("The post tags [" + postMetadata.tags.join() + "] does not include " + tag + "."));
-                }
-                resolve(true);
-            }
-            else if (this.mode === TagsRule.Mode.ANY) { // the post should have at least one of the specified tags
-                for (let i = 0; i < this.tags.length; i++) {
-                    const tag = this.tags[i];
-                    if (postMetadata.tags.indexOf(tag) !== -1) {
-                        resolve(true);
-                        return;
+                if (this.mode === TagsRule.Mode.ALLOW) { // allow mode (every post tag must be within this list)
+                    for (let i = 0; i < postMetadata.tags.length; i++) {
+                        const tag = postMetadata.tags[i];
+                        if (this.tags.indexOf(tag) === -1)
+                                reject(new ValidationError("Tag " + tag + " is not on the allowed tags list [" + this.tags.join() + "]."));
                     }
+                    resolve(true);
                 }
-                reject(new ValidationError("None of the tags [" + postMetadata.tags.join() + "] is on the \"require\" tags list [" + this.tags.join() + "]."));
-            }
-            else reject(new ValidationError("Unknown mode in tags this."));
+                else if (this.mode === TagsRule.Mode.DENY) { // deny mode (none of post tags can be on this list)
+                    for (let i = 0; i < postMetadata.tags.length; i++) {
+                        const tag = postMetadata.tags[i];
+                        if (this.tags.indexOf(tag) !== -1)
+                                reject(new ValidationError("Tag " + tag + " is on the denied tags list [" + this.tags.join() + "]."));
+                    }
+                    resolve(true);
+                }
+                else if (this.mode === TagsRule.Mode.REQUIRE) { // the post should have all of the specified tags
+                    for (let i = 0; i < this.tags.length; i++) {
+                        const tag = this.tags[i];
+                        if (postMetadata.tags.indexOf(tag) === -1)
+                            reject(new ValidationError("The post tags [" + postMetadata.tags.join() + "] does not include " + tag + "."));
+                    }
+                    resolve(true);
+                }
+                else if (this.mode === TagsRule.Mode.ANY) { // the post should have at least one of the specified tags
+                    for (let i = 0; i < this.tags.length; i++) {
+                        const tag = this.tags[i];
+                        if (postMetadata.tags.indexOf(tag) !== -1) {
+                            resolve(true);
+                            return;
+                        }
+                    }
+                    reject(new ValidationError("None of the tags [" + postMetadata.tags.join() + "] is on the \"require\" tags list [" + this.tags.join() + "]."));
+                }
+                else reject(new ValidationError("Unknown mode in tags this."));
+            });
         });
     }
 
