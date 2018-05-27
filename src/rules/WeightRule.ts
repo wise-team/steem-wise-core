f1a77fb4c94d3d1295465fda0793190cdd0ece35
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-05-27 22:07:37 +0200

    Partially implement WeightRule

--- src/rules/WeightRule.ts
+++ src/rules/WeightRule.ts
@@ -2,6 +2,8 @@ import { Rule } from "./Rule";
 import { SmartvotesOperation } from "../protocol/SmartvotesOperation";
 import { ValidationError } from "../validation/ValidationError";
 import { ValidationContext } from "../validation/ValidationContext";
+import { Promise } from "bluebird";
+import { SendVoteorder } from "../protocol/SendVoteorder";
 
 export class WeightRule extends Rule {
     public min: number;
@@ -20,12 +22,16 @@ export class WeightRule extends Rule {
         return Rule.Type.Weight;
     }
 
-    public validate (
-        op: SmartvotesOperation,
-        context: ValidationContext,
-        callback: (error: Error | undefined, result: ValidationError | true) => void
-    ): void {
-        throw new Error("Not implemented yet");
+    public validate (voteorder: SendVoteorder, context: ValidationContext): Promise<true> {
+        return new Promise((resolve, reject) => {
+            if (this.mode === WeightRule.Mode.SINGLE_VOTE_WEIGHT) {
+                return voteorder.weight >= this.min && voteorder.weight <= this.max;
+            }
+            else if (this.mode === WeightRule.Mode.VOTES_PER_DAY) {
+                throw new Error("Not implemented yet"); // TODO
+            }
+            else throw new Error("Unknown WeightRule.mode");
+        });
     }
 
 }
