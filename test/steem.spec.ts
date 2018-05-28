762b5e8c89468e4e627fa10666d826cf6d156be5
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-05-28 20:21:07 +0200

    Fix test steem.spec.ts

--- test/steem.spec.ts
+++ test/steem.spec.ts
@@ -2,9 +2,6 @@ import { expect } from "chai";
 import "mocha";
 import * as steem from "steem";
 
-import { RawOperation } from "../src/blockchain/blockchain-operations-types";
-import { SteemSmartvotes, SteemOperationNumber } from "../src/steem-smartvotes";
-
 describe("test/steem.spec.ts", () => {
     describe("account_history_api.get_account_history", () => {
         it("Returns incorrect op_in_trx", function(done) {
@@ -23,7 +20,7 @@ describe("test/steem.spec.ts", () => {
                          * it is very important to replace all occurances of lesserThan_solveOpInTrxBug to lesserThan,
                          * when the bug is solved.
                          */
-                        const rawOp: RawOperation = result[0] as RawOperation;
+                        const rawOp: [string, {op_in_trx: number}] = result[0] as [string, {op_in_trx: number}];
                         if (rawOp[1].op_in_trx === 3) done(new Error("Steem account_history_api returns correct op_in_trx (" + rawOp[1].op_in_trx + "). The bug was repaird. Please replace all lesserThan_solveOpInTrxBug to lesserThan."));
                         else {
                             console.log("Steem account_history_api returns wrong op_in_trx (" + rawOp[1].op_in_trx + "). The bug still persists, but is patched in this app, so there is nothing to do.");
