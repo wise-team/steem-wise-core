a601465e74950a57cc13fa11e842285f4f8d517f
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-05-30 16:07:42 +0200

    Add #getDynamicGlobalProperties, #getVoterInfo, #getDelegatorInfo to ValidationContext

--- src/validation/ValidationContext.ts
+++ src/validation/ValidationContext.ts
@@ -2,6 +2,8 @@ import { SteemPost } from "../blockchain/SteemPost";
 import { SendVoteorder } from "../protocol/SendVoteorder";
 import { Api } from "../api/Api";
 import { OneTimePromise } from "../util/OneTimePromise";
+import { DynamicGlobalProperties } from "../blockchain/DynamicGlobalProperties";
+import { AccountInfo } from "../blockchain/AccountInfo";
 
 export class ValidationContext {
     private api: Api;
@@ -9,6 +11,9 @@ export class ValidationContext {
     private voter: string;
     private voteorder: SendVoteorder;
     private postLoader = new OneTimePromise<SteemPost>(100 * 1000);
+    private dgpLoader = new OneTimePromise<DynamicGlobalProperties>(100 * 1000);
+    private voterInfoLoader = new OneTimePromise<AccountInfo>(100 * 1000);
+    private delegatorInfoLoader = new OneTimePromise<AccountInfo>(100 * 1000);
 
     public constructor(api: Api, delegator: string, voter: string, voteorder: SendVoteorder) {
         this.api = api;
@@ -19,5 +24,17 @@ export class ValidationContext {
 
     public getPost(): Promise<SteemPost> {
         return this.postLoader.execute(this.api.loadPost(this.voteorder.author, this.voteorder.permlink));
+    }
+
+    public getDynamicGlobalProperties(): Promise<DynamicGlobalProperties> {
+        return this.dgpLoader.execute(this.api.getDynamicGlobalProperties());
+    }
+
+    public getVoterInfo(): Promise<AccountInfo> {
+        return this.voterInfoLoader.execute(this.api.getAccountInfo(this.voter));
+    }
+
+    public getDelegatorInfo(): Promise<AccountInfo> {
+        return this.delegatorInfoLoader.execute(this.api.getAccountInfo(this.delegator));
     }
 }
