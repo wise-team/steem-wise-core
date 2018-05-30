887cb4d63e2790d2e537d42f4388e21b95a2d343
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-05-30 18:19:47 +0200

    Draft synchronization test

--- test/synchronization.spec.ts
+++ test/synchronization.spec.ts
@@ -1,1 +1,94 @@
 // TODO test Synchronizator with FakeApi
+
+import { expect, assert } from "chai";
+import { Promise } from "bluebird";
+import "mocha";
+
+import { Wise, SteemOperationNumber, SendVoteorder } from "../src/wise";
+import { FakeApi } from "./util/FakeApi";
+import { SteemPost } from "../src/blockchain/SteemPost";
+
+/**
+ * Setup
+ */
+const voter = "voter123";
+const delegator = "delegator456";
+const delegatorWise = new Wise(delegator, new FakeApi());
+const voterWise = new Wise(delegator, new FakeApi());
+let syncRunning = true;
+
+/**
+ * Fake posts
+ */
+const posts: SteemPost [] = [
+    {
+        id: 1,
+        author: "a1",
+        permlink: "p1",
+        category: "cat1",
+        title: "title1",
+        body: "body1",
+        json_metadata: JSON.stringify({tags: ["tag1", "tag2"]}),
+        last_update: "",
+        created: "",
+        active: "",
+        last_payout: ""
+    }
+];
+
+/**
+ * Testing sequence
+ */
+const sequence: ([string, () => Promise<void>])[] = [];
+
+sequence.push(["delegator starts synchronization", () => {
+    return new Promise((resolve, reject) => {
+        delegatorWise.runSynchronizerLoop(new SteemOperationNumber(0, 0, 0), (error: Error | undefined, message: string, moment: SteemOperationNumber): boolean => {
+            if (error) {
+                reject();
+                return false;
+            }
+            else {
+                if (!syncRunning) resolve();
+                return syncRunning;
+            }
+        });
+    });
+}]);
+
+sequence.push(["voter sends voteorder before rules", () => {
+    return new Promise<SteemOperationNumber>((resolve, reject) => {
+        const skipValidation = true;
+        const vo: SendVoteorder = {
+            rulesetName: "",
+            author: posts[0].author,
+            permlink: posts[0].permlink,
+            weight: 10000
+        };
+        voterWise.sendVoteorder(delegator, vo, (error: Error | undefined, result: SteemOperationNumber | undefined): void => {
+            if (error) reject(error);
+            else resolve(result);
+        }, () => {}, skipValidation);
+    })
+    .then((son: SteemOperationNumber) => {
+        expect(son.blockNum).to.be.greaterThan(0);
+    });
+}]);
+
+sequence.push(["Stop sync", () => {
+    return new Promise((resolve, reject) => {
+        syncRunning = false;
+        resolve();
+    });
+}]);
+
+/**
+ * Run!
+ */
+describe("test/index.spec.ts", () => {
+    describe("Synchronizer", function() {
+        it("Synchronizes everything perfectly", () => {
+            return Promise.resolve(sequence).mapSeries((item: [string, () => Promise<void>]) => item[1]());
+        });
+    });
+});
--- test/util/FakeApi.ts
+++ test/util/FakeApi.ts
@@ -3,13 +3,16 @@ import * as fs from "fs";
 import * as path from "path";
 
 import { SteemPost } from "../../src/blockchain/SteemPost";
-import { SetRules } from "../../src/protocol/SetRules";
+import { SetRules, EffectuatedSetRules } from "../../src/protocol/SetRules";
 import { SteemOperationNumber } from "../../src/blockchain/SteemOperationNumber";
 import { ChainableSupplier } from "../../src/chainable/Chainable";
 import { SteemOperation } from "../../src/blockchain/SteemOperation";
 import { Api } from "../../src/api/Api";
 import { Protocol } from "../../src/protocol/Protocol";
 import { DirectBlockchainApi } from "../../src/api/directblockchain/DirectBlockchainApi";
+import { EffectuatedSmartvotesOperation } from "../../src/protocol/EffectuatedSmartvotesOperation";
+import { DynamicGlobalProperties } from "../../src/blockchain/DynamicGlobalProperties";
+import { AccountInfo } from "../../src/blockchain/AccountInfo";
 
 // TODO implement and use in tests
 export class FakeApi extends Api {
@@ -17,7 +20,7 @@ export class FakeApi extends Api {
     public constructor() {
         super();
 
-        this.data = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../data/operations/fake-blockchain.operations.json")));
+        this.data = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../data/operations/fake-blockchain.operations.json"), "UTF-8"));
     }
 
     public name(): string {
@@ -32,20 +35,27 @@ export class FakeApi extends Api {
         return new Promise((resolve, reject) => reject(new Error("This api is disabled")));
     }
 
-    public streamSince(moment: SteemOperationNumber): ChainableSupplier<SteemOperation, any> {
-        throw new Error("This api is disabled");
-    }
-
     public sendToBlockchain(operations: [string, object][]): Promise<SteemOperationNumber> {
         return new Promise((resolve, reject) => reject(new Error("This api is disabled")));
     }
 
-    private getAccountOperations(username: string): any {
-        for (const acc of this.data) {
-            if (acc[0] === username) {
-                return acc[1];
-            }
-        }
-        throw new Error("No such account in fake blockchain");
+    public loadAllRulesets(delegator: string, at: SteemOperationNumber, protocol: Protocol): Promise<EffectuatedSetRules []> {
+        return new Promise((resolve, reject) => reject(new Error("This api is disabled")));
+    }
+
+    public getLastConfirmationMoment(delegator: string): Promise<SteemOperationNumber> {
+        return new Promise((resolve, reject) => reject(new Error("This api is disabled")));
+    }
+
+    public getWiseOperationsRelatedToDelegatorInBlock(delegator: string, blockNum: number): Promise<EffectuatedSmartvotesOperation []> {
+        return new Promise((resolve, reject) => reject(new Error("This api is disabled")));
+    }
+
+    public getDynamicGlobalProperties(): Promise<DynamicGlobalProperties> {
+        return new Promise((resolve, reject) => reject(new Error("This api is disabled")));
+    }
+
+    public getAccountInfo(username: string): Promise<AccountInfo> {
+        return new Promise((resolve, reject) => reject(new Error("This api is disabled")));
     }
 }
