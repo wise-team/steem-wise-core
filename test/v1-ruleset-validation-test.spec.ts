f29fa30dc4c52ba86e245e2646cf4ef48b2483e0
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-06-03 14:49:05 +0200

    Fix test v1-ruleset.validation-test.spec.ts #throws NoSuchPost on non existing post

--- test/v1-ruleset-validation-test.spec.ts
+++ test/v1-ruleset-validation-test.spec.ts
@@ -22,14 +22,14 @@ const validVoteorder: SendVoteorder = {
 
 const rulesetMomentForValidation: SteemOperationNumber = new SteemOperationNumber(22144059, 32, 0).addTransactions(1); // Moment just after tx_id = 7fe4a1a4efadb1230c41c2c865df15d91eb3c452
 
-describe("test/ruleset-validation.spec.ts", function() {
+describe("test/v1-ruleset-validation-test.spec.ts", function() {
     describe("RulesValidator.validateVoteOrder [delegator=steemprojects1, voter=guest123]", function() {
         this.retries(1);
 
         const wise = new Wise(voter, FakeApi.fromDataset(fakeDataset));
 
         it("passes valid voteorder", function(done) {
-            this.timeout(10000);
+            this.timeout(200);
 
             const voteorder = validVoteorder;
 
@@ -66,9 +66,9 @@ describe("test/ruleset-validation.spec.ts", function() {
             });
         });
 
-        [-1, 0, undefined, NaN, 3, 10000, Infinity].forEach(function(weight) {
+        [-1, 0, undefined, NaN, 3, 200, Infinity].forEach(function(weight) {
             it("fails on invald weight (" + weight + ")", function(done) {
-                this.timeout(10000);
+                this.timeout(200);
                 const voteorder: SendVoteorder = Util.objectAssign({}, validVoteorder, { rulesetName: steemprojects1Rulesets.upvoteNoRulesMaxWeight2.name, weight: weight });
 
                 wise.validateVoteorder(delegator, voter, voteorder, rulesetMomentForValidation, function(error: Error | undefined, result: true | ValidationException | undefined) {
@@ -81,7 +81,7 @@ describe("test/ruleset-validation.spec.ts", function() {
 
 
         it("allows valid weight (2)", function(done) {
-            this.timeout(10000);
+            this.timeout(200);
             const voteorder: SendVoteorder = Util.objectAssign({}, validVoteorder, { rulesetName: steemprojects1Rulesets.upvoteNoRulesMaxWeight2.name, weight: 2 });
 
             wise.validateVoteorder(delegator, voter, voteorder, rulesetMomentForValidation, function(error: Error | undefined, result: true | ValidationException | undefined) {
@@ -92,7 +92,7 @@ describe("test/ruleset-validation.spec.ts", function() {
         });
 
         it("fails on nonexistent ruleset", function(done) {
-            this.timeout(10000);
+            this.timeout(200);
             const voteorder: SendVoteorder = Util.objectAssign({}, validVoteorder, { rulesetName: "NonExistent" + Date.now() });
 
             wise.validateVoteorder(delegator, voter, voteorder, rulesetMomentForValidation, function(error: Error | undefined, result: true | ValidationException | undefined) {
@@ -103,7 +103,7 @@ describe("test/ruleset-validation.spec.ts", function() {
         });
 
         it("fails on different voter", function(done) {
-            this.timeout(10000);
+            this.timeout(200);
             const voteorder: SendVoteorder = validVoteorder;
 
             wise.validateVoteorder(delegator, "NonExistent-voter-" + Date.now(), voteorder, rulesetMomentForValidation, function(error: Error | undefined, result: true | ValidationException | undefined) {
@@ -223,7 +223,7 @@ describe("test/ruleset-validation.spec.ts", function() {
         ].forEach(function(voteorderCase) {
             it((voteorderCase.pass ? "passes when all rules fulfilled" : "fails when not all rules fulfilled") + " [ruleset=\"" + voteorderCase.ruleset.name + "\","
             + " post=@" + voteorderCase.author + "/" + voteorderCase.permlink + "]", function(done) {
-                this.timeout(10000);
+                this.timeout(200);
                 const voteorder: SendVoteorder = Util.objectAssign({}, validVoteorder, { rulesetName: voteorderCase.ruleset.name, author: voteorderCase.author, permlink: voteorderCase.permlink });
                 wise.validateVoteorder(delegator, voter, voteorder, rulesetMomentForValidation, function(error: Error | undefined, result: true | ValidationException | undefined) {
                     if (voteorderCase.pass) {
@@ -240,13 +240,13 @@ describe("test/ruleset-validation.spec.ts", function() {
             });
         });
 
-        it("throws NoSuchPost on non existing post", function(done) {
-            this.timeout(10000);
+        it("throws ValidationException on non existing post", function(done) {
+            this.timeout(200);
             const voteorder: SendVoteorder = Util.objectAssign({}, validVoteorder,
                 { rulesetName: steemprojects1Rulesets.upvoteAllowAuthorNoisy.name,
                     author: "noisy", permlink: "Non-existing-post" + Date.now() }); // author is correct, but post doesnt exist => fail
             wise.validateVoteorder(delegator, voter, voteorder, rulesetMomentForValidation, function(error: Error | undefined, result: true | ValidationException | undefined) {
-                if ((error as ValidationException).validationException) done();
+                if (error && (error as ValidationException).validationException) done();
                 else if (result === true) done(new Error("Should fail on non existing post"));
                 else done(error);
             });
