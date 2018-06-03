a7f4b1b2ede00ae057b9c23dfbc699ee8f50a4e9
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-06-03 13:44:14 +0200

    Test OneTimePromise rejections

--- test/util.spec.ts
+++ test/util.spec.ts
@@ -17,7 +17,7 @@ describe("test/util.spec.ts", () => {
                 });
             };
 
-            const loader = new OneTimePromise<number>(100 * 1000);
+            const loader = new OneTimePromise<number>(100);
 
             const checkerFn = () => {
                 loader.execute(promiseReturner)
@@ -27,6 +27,7 @@ describe("test/util.spec.ts", () => {
                 .catch((error: Error) => done(error));
             };
 
+            checkerFn();
             setTimeout(checkerFn, 5);
             setTimeout(checkerFn, 6);
             setTimeout(checkerFn, 10);
@@ -35,6 +36,71 @@ describe("test/util.spec.ts", () => {
             setTimeout(() => {
                 if (calls === 1) done();
                 else done(new Error("Num of calls does not equal 1 (calls = " + calls + ")"));
+            }, 25);
+        });
+
+        it("rejects on explicit rejections", (done) => {
+            let numOfRejects = 0;
+            let numOfResolves = 0;
+            let numOfCalls = 0;
+            const promiseReturner = () => {
+                return new Promise<number>((resolve, reject) => {
+                    setTimeout(() => {
+                        numOfCalls++;
+                        reject(new Error("Some error"));
+                    }, 10);
+                });
+            };
+
+            const loader = new OneTimePromise<number>(100);
+
+            const checkerFn = () => {
+                loader.execute(promiseReturner)
+                .then((numOfCalls: number) => {
+                    numOfResolves++;
+                    done(new Error("Resolves without error"));
+                }, e => {
+                    { numOfRejects++; }
+                });
+            };
+
+            checkerFn();
+            setTimeout(checkerFn, 5);
+            setTimeout(checkerFn, 10);
+            setTimeout(checkerFn, 15);
+
+            setTimeout(() => {
+                if (numOfRejects == 4 && numOfResolves == 0 && numOfCalls == 1) done();
+                else done(new Error("Not all promises were rejected (rejects=" + numOfRejects + ", resolves=" + numOfResolves + ", calls=" + numOfCalls + ")"));
+            }, 25);
+        });
+
+        it("rejects on thrown errors", (done) => {
+            let numOfRejects = 0;
+            const promiseReturner = () => {
+                return new Promise<number>((resolve, reject) => {
+                    throw new Error("Some error");
+                });
+            };
+
+            const loader = new OneTimePromise<number>(100);
+
+            const checkerFn = () => {
+                loader.execute(promiseReturner)
+                .then((numOfCalls: number) => {
+                    done(new Error("Resolves without error"));
+                })
+                .catch((error: Error) => { numOfRejects++; });
+            };
+
+            checkerFn();
+            setTimeout(checkerFn, 5);
+            setTimeout(checkerFn, 10);
+            setTimeout(checkerFn, 15);
+
+            setTimeout(() => {
+                if (numOfRejects == 4) done();
+                else done(new Error("Not all promises were rejected (" + numOfRejects + ")"));
             }, 25);
         });
     });
