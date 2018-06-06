71559b65f452b38250649e56e16a9d64ad54318f
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-06-06 16:02:10 +0200

    Test weight rule

--- test/rules.spec.ts
+++ test/rules.spec.ts
@@ -1,13 +1,15 @@
 import { expect, assert } from "chai";
 import { Promise } from "bluebird";
 import "mocha";
-import { AuthorsRule, SendVoteorder, Wise, ValidationException, TagsRule, WeightRule, CustomRPCRule } from "../src/wise";
+import * as _ from "lodash";
 
-import * as fakeDataset_ from "./data/fake-blockchain.json";
-import { FakeApi } from "../src/api/FakeApi";
+import { AuthorsRule, SendVoteorder, Wise, ValidationException, TagsRule, WeightRule, CustomRPCRule } from "../src/wise";
 import { ValidationContext } from "../src/validation/ValidationContext";
 import { Rule } from "../src/rules/Rule";
 import { RulePrototyper } from "../src/rules/RulePrototyper";
+
+import { FakeApi } from "../src/api/FakeApi";
+import * as fakeDataset_ from "./data/fake-blockchain.json";
 const fakeDataset = fakeDataset_ as object as FakeApi.Dataset;
 
 const delegator = "noisy";
@@ -146,6 +148,69 @@ describe("test/util.spec.ts", () => {
         });
     });
 
+    describe("WeightRule", () => {
+        const voteorder: SendVoteorder = {
+            rulesetName: "",
+            weight: 1,
+            author: "noisy",
+            permlink: "nonexistent-post-" + Date.now()
+        };
+        const context = new ValidationContext(fakeApi, delegator, voter, voteorder);
+
+        describe("mode = SINGLE_VOTE_WEIGHT", () => {
+            it ("allows 0 <= 50 <= 100", () => {
+                const rule = new WeightRule(WeightRule.Mode.SINGLE_VOTE_WEIGHT, 0, 100);
+                const vo = _.set(_.cloneDeep(voteorder), "weight", 50);
+                return rule.validate(vo, context);
+            });
+
+            it ("allows 0 <= 0 <= 100", () => {
+                const rule = new WeightRule(WeightRule.Mode.SINGLE_VOTE_WEIGHT, 0, 100);
+                const vo = _.set(_.cloneDeep(voteorder), "weight", 0);
+                return rule.validate(vo, context);
+            });
+
+            it ("allows 0 <= 100 <= 100", () => {
+                const rule = new WeightRule(WeightRule.Mode.SINGLE_VOTE_WEIGHT, 0, 100);
+                const vo = _.set(_.cloneDeep(voteorder), "weight", 0);
+                return rule.validate(vo, context);
+            });
+
+            it ("allows -100 <= 0 <= 100", () => {
+                const rule = new WeightRule(WeightRule.Mode.SINGLE_VOTE_WEIGHT, -100, 100);
+                const vo = _.set(_.cloneDeep(voteorder), "weight", 0);
+                return rule.validate(vo, context);
+            });
+
+            it ("allows -100 <= -100 <= 100", () => {
+                const rule = new WeightRule(WeightRule.Mode.SINGLE_VOTE_WEIGHT, -100, 100);
+                const vo = _.set(_.cloneDeep(voteorder), "weight", -100);
+                return rule.validate(vo, context);
+            });
+
+            it ("rejects -100 <= -101 <= 100", () => {
+                const rule = new WeightRule(WeightRule.Mode.SINGLE_VOTE_WEIGHT, -100, 100);
+                const vo = _.set(_.cloneDeep(voteorder), "weight", -101);
+                return rule.validate(vo, context)
+                .then(() => { throw new Error("Should fail"); }, () => {});
+            });
+
+            it ("rejects -100 <= 101 <= 100", () => {
+                const rule = new WeightRule(WeightRule.Mode.SINGLE_VOTE_WEIGHT, -100, 100);
+                const vo = _.set(_.cloneDeep(voteorder), "weight", 101);
+                return rule.validate(vo, context)
+                .then(() => { throw new Error("Should fail"); }, () => {});
+            });
+
+            it ("rejects 10 <= 0 <= 100", () => {
+                const rule = new WeightRule(WeightRule.Mode.SINGLE_VOTE_WEIGHT, 10, 100);
+                const vo = _.set(_.cloneDeep(voteorder), "weight", 0);
+                return rule.validate(vo, context)
+                .then(() => { throw new Error("Should fail"); }, () => {});
+            });
+        });
+    });
+
     describe("RulePrototyper", () => {
         it ("Unserialized rules are equal to serialized after prototyping", () => {
             const rulesPrimary: Rule [] = [
@@ -173,6 +238,5 @@ describe("test/util.spec.ts", () => {
         });
     });
 
-    // TODO test WeightRule
     // TODO test CustomRPCRule
 });
