be8728772883cc85d884489c0c9071b2f9f0ac42
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-07-24 19:19:41 +0200

    Repair api tests

--- test/api.spec.ts
+++ test/api.spec.ts
@@ -192,13 +192,13 @@ describe("test/api.spec.ts", function () {
             });
 
             it("Loads wise operations sent by voter but refering to delegator", () => {
-                return api.getWiseOperationsRelatedToDelegatorInBlock("steemprojects3", 22484096, wise.getProtocol())
+                const blockNum = 23487915;
+                return api.getWiseOperationsRelatedToDelegatorInBlock("steemprojects3", blockNum, wise.getProtocol())
                 .then((ops: EffectuatedSmartvotesOperation []) => {
                     expect(ops).to.be.an("array").with.length(1);
-                    expect(ops[0].moment.blockNum, "ops[0] block_num").to.equal(22484096);
-                    if (api.name() !== "FakeApi")  expect(ops[0].moment.operationNum, "ops[0] operation_num").to.equal(1);
+                    expect(ops[0].moment.blockNum, "ops[0] block_num").to.equal(blockNum);
                     expect(ops[0].delegator, "ops[0].delegator").to.equal("steemprojects3");
-                    expect(ops[0].voter, "ops[0].voter").to.equal("guest123");
+                    expect(ops[0].voter, "ops[0].voter").to.equal("steemprojects1");
                 });
             });
 
