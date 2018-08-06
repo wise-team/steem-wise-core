054daf1d69ed00f79113df41005c178f80542746
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-08-06 19:56:54 +0200

    Add branch guard to semver script

--- scripts/semver.sh
+++ scripts/semver.sh
@@ -11,6 +11,13 @@ if [ -z "${VERSION}" ]; then
 fi
 
 
+REQUIRED_BRANCH="master"
+if [ "$(git rev-parse --abbrev-ref HEAD)" != "${REQUIRED_BRANCH}" ]; then
+    echo "You must be on a \"${REQUIRED_BRANCH}\" branch to do semver"
+    exit 1
+fi
+
+
 echo "Updating steem-wise-core to ${VERSION}"
 node -e " \
 var packageFileContents = require(\"./package.json\"); \
@@ -28,6 +35,7 @@ echo "Build successful"
 echo "Creating git tag"
 git add package.json package-lock.json
 git commit -m "Semver ${VERSION}"
+git push
 git tag -a "v${VERSION}" -m "Steem WISE core library version ${VERSION}"
 git push --tags
 echo "Done creating tag"
