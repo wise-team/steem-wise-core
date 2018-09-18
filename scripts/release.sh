f46ee0560ce717f886b94c6b4d8f12e4e366d595
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-09-18 17:45:41 +0200

    build(semver script): add github release code

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,13 +3,12 @@ set -e # fail on first error
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.." # parent dir of scripts dir
 cd "${DIR}"
 
-VERSION=$1
 
+VERSION=$1
 if [ -z "${VERSION}" ]; then
     echo "Error: You must specify new version"
     exit 1
 fi
-
 
 REQUIRED_BRANCH="master"
 if [ "$(git rev-parse --abbrev-ref HEAD)" != "${REQUIRED_BRANCH}" ]; then
@@ -19,12 +18,18 @@ else
     echo "On master branch"
 fi
 
-if [[ "$(node --version)" = "$(cat .nvmrc)"* ]]; then \
-       echo "Node version correct ($(node --version))"; 
+if [[ "$(node --version)" = "$(cat .nvmrc)"* ]]; then
+    echo "Node version correct ($(node --version))"
 else 
     echo "Error: Node version does not match .nvmrc"
     exit 1
 fi
+
+if [[ -z "${CONVENTIONAL_GITHUB_RELEASER_TOKEN}" ]]; then
+    echo "You need to set up CONVENTIONAL_GITHUB_RELEASER_TOKEN env. It should contain github token with scope \"public_repo\"."
+    exit 1;
+fi
+
 
 echo "Building..."
 npm install
@@ -64,6 +69,12 @@ echo "Done creating tag"
 
 echo "Publishing to npmjs.com registry"
 npm publish
+echo "Done publishing"
+
+
+echo "Creating github release"
+conventional-github-releaser -p angular
+echo "Github release done"
 
 
 echo "Done"
