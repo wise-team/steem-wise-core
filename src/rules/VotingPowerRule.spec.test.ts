21a69c6712c61ebf3e563abd7a979fd6b4ba1427
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-03-08 17:16:10 +0100

    fix(voting power rule): limit value wise-team/wise-hub#37

--- src/rules/VotingPowerRule.spec.test.ts
+++ src/rules/VotingPowerRule.spec.test.ts
@@ -1,11 +1,10 @@
 // 3rd party imports
 import { expect } from "chai";
 import "mocha";
-import { Log } from "../log/Log";
 import * as steem from "steem";
 
 // wise imports
-import { VotingPowerRule, SendVoteorder, ValidationException, Wise } from "../wise";
+import { VotingPowerRule, SendVoteorder, ValidationException } from "../wise";
 import { ValidationContext } from "../validation/ValidationContext";
 import { FakeWiseFactory } from "../_test/util/FakeWiseFactory";
 import {
@@ -18,122 +17,139 @@ import {
 const voter = "perduta";
 const fakeDataset = FakeWiseFactory.loadDataset();
 const fakeApi = FakeWiseFactory.buildFakeApiWithDataset(fakeDataset);
-const wise = new Wise(voter, fakeApi);
 
-describe("test/unit/rule-votingpower.spec.ts", () => {
-    describe("VotingPowerRule", function() {
-        const tests: { mode: VotingPowerRule.Mode; ruleValue: number; testValue: number; pass: boolean }[] = [
-            {
-                mode: VotingPowerRule.Mode.EQUAL,
-                ruleValue: 1000,
-                testValue: 1000,
-                pass: true,
-            },
-            {
-                mode: VotingPowerRule.Mode.EQUAL,
-                ruleValue: 1000,
-                testValue: 999,
-                pass: false,
-            },
-            {
-                mode: VotingPowerRule.Mode.EQUAL,
-                ruleValue: 1000,
-                testValue: 1001,
-                pass: false,
-            },
-            {
-                mode: VotingPowerRule.Mode.MORE_THAN,
-                ruleValue: 1000,
-                testValue: 1000,
-                pass: false,
-            },
-            {
-                mode: VotingPowerRule.Mode.MORE_THAN,
-                ruleValue: 1000,
-                testValue: 1001,
-                pass: true,
-            },
-            {
-                mode: VotingPowerRule.Mode.MORE_THAN,
-                ruleValue: 1000,
-                testValue: 999,
-                pass: false,
-            },
-            {
-                mode: VotingPowerRule.Mode.LESS_THAN,
-                ruleValue: 1000,
-                testValue: 1000,
-                pass: false,
-            },
-            {
-                mode: VotingPowerRule.Mode.LESS_THAN,
-                ruleValue: 1000,
-                testValue: 1001,
-                pass: false,
-            },
-            {
-                mode: VotingPowerRule.Mode.LESS_THAN,
-                ruleValue: 1000,
-                testValue: 999,
-                pass: true,
-            },
-        ];
+describe.only("VotingPowerRule", function() {
+    describe("#validateRuleObject", () => {
+        it("does not allow voting power greater than 100% (10000)", () => {
+            const rule = new VotingPowerRule(VotingPowerRule.Mode.EQUAL, 10001);
+            expect(() => rule.validateRuleObject(rule))
+                .to.throw()
+                .that.haveOwnProperty("message")
+                .that.contains("greater")
+                .and.contains("100%");
+        });
 
-        tests.forEach((testMode, i: number) =>
-            it(
-                "VotingPowerRule " +
-                    (testMode.pass ? "should pass" : "should fail") +
-                    ": " +
-                    testMode.testValue +
-                    " " +
-                    testMode.mode +
-                    " " +
-                    testMode.ruleValue,
-                () => {
-                    const rule = new VotingPowerRule(testMode.mode, testMode.ruleValue);
-                    const delegatorAI = {
-                        name: "delegator-" + i,
-                        voting_power: testMode.testValue,
-                    };
-                    fakeDataset.accounts.push((delegatorAI as object) as steem.AccountInfo);
-                    const voteorder: SendVoteorder = {
-                        rulesetName: "",
-                        weight: 1,
-                        author: "noisy",
-                        permlink:
-                            "dear-whales-please-consider-declining-all-comment-rewards-by-default-in-settings-5-reasons-to-do-that",
-                    };
-                    const context = new ValidationContext(fakeApi, delegatorAI.name, voter, voteorder);
+        it("does not allow voting power less than 0% (0)", () => {
+            const rule = new VotingPowerRule(VotingPowerRule.Mode.EQUAL, -1);
+            expect(() => rule.validateRuleObject(rule))
+                .to.throw()
+                .that.haveOwnProperty("message")
+                .that.contains("less")
+                .and.contains("0%");
+        });
+    });
 
-                    return rule.validate(voteorder, context).then(
-                        () => {
-                            // passed
-                            if (!testMode.pass) throw new Error("Should fail");
-                        },
-                        error => {
-                            // failed
-                            if (testMode.pass) throw error;
-                            else {
-                                if (!(error as ValidationException).validationException)
-                                    throw new Error("Should fail with ValidationException");
-                            }
+    const tests: { mode: VotingPowerRule.Mode; ruleValue: number; testValue: number; pass: boolean }[] = [
+        {
+            mode: VotingPowerRule.Mode.EQUAL,
+            ruleValue: 1000,
+            testValue: 1000,
+            pass: true,
+        },
+        {
+            mode: VotingPowerRule.Mode.EQUAL,
+            ruleValue: 1000,
+            testValue: 999,
+            pass: false,
+        },
+        {
+            mode: VotingPowerRule.Mode.EQUAL,
+            ruleValue: 1000,
+            testValue: 1001,
+            pass: false,
+        },
+        {
+            mode: VotingPowerRule.Mode.MORE_THAN,
+            ruleValue: 1000,
+            testValue: 1000,
+            pass: false,
+        },
+        {
+            mode: VotingPowerRule.Mode.MORE_THAN,
+            ruleValue: 1000,
+            testValue: 1001,
+            pass: true,
+        },
+        {
+            mode: VotingPowerRule.Mode.MORE_THAN,
+            ruleValue: 1000,
+            testValue: 999,
+            pass: false,
+        },
+        {
+            mode: VotingPowerRule.Mode.LESS_THAN,
+            ruleValue: 1000,
+            testValue: 1000,
+            pass: false,
+        },
+        {
+            mode: VotingPowerRule.Mode.LESS_THAN,
+            ruleValue: 1000,
+            testValue: 1001,
+            pass: false,
+        },
+        {
+            mode: VotingPowerRule.Mode.LESS_THAN,
+            ruleValue: 1000,
+            testValue: 999,
+            pass: true,
+        },
+    ];
+
+    tests.forEach((testMode, i: number) => {
+        it(
+            "VotingPowerRule " +
+                (testMode.pass ? "should pass" : "should fail") +
+                ": " +
+                testMode.testValue +
+                " " +
+                testMode.mode +
+                " " +
+                testMode.ruleValue,
+            () => {
+                const rule = new VotingPowerRule(testMode.mode, testMode.ruleValue);
+                const delegatorAI = {
+                    name: "delegator-" + i,
+                    voting_power: testMode.testValue,
+                };
+                fakeDataset.accounts.push((delegatorAI as object) as steem.AccountInfo);
+                const voteorder: SendVoteorder = {
+                    rulesetName: "",
+                    weight: 1,
+                    author: "noisy",
+                    permlink:
+                        "dear-whales-please-consider-declining-all-comment-rewards-by-default-in-settings-5-reasons-to-do-that",
+                };
+                const context = new ValidationContext(fakeApi, delegatorAI.name, voter, voteorder);
+
+                return rule.validate(voteorder, context).then(
+                    () => {
+                        // passed
+                        if (!testMode.pass) throw new Error("Should fail");
+                    },
+                    error => {
+                        // failed
+                        if (testMode.pass) throw error;
+                        else {
+                            if (!(error as ValidationException).validationException)
+                                throw new Error("Should fail with ValidationException");
                         }
-                    );
-                }
-            )
-        );
-
-        tests.forEach((test, i: number) =>
-            it("is correctly serialized and deserialized by v2", () => {
-                const rule = new VotingPowerRule(test.mode, test.ruleValue);
-                const encoded: wise_rule_voting_power = wise_rule_voting_power_encode(rule);
-
-                const decoded: VotingPowerRule = wise_rule_voting_power_decode(encoded);
-                expect(decoded).to.deep.equal(rule);
-
-                const encoded2: wise_rule_voting_power = wise_rule_voting_power_encode(decoded);
-                expect(encoded2).to.deep.equal(encoded);
-            })
+                    }
+                );
+            }
         );
     });
+
+    tests.forEach((test, i: number) =>
+        it("is correctly serialized and deserialized by v2", () => {
+            const rule = new VotingPowerRule(test.mode, test.ruleValue);
+            const encoded: wise_rule_voting_power = wise_rule_voting_power_encode(rule);
+
+            const decoded: VotingPowerRule = wise_rule_voting_power_decode(encoded);
+            expect(decoded).to.deep.equal(rule);
+
+            const encoded2: wise_rule_voting_power = wise_rule_voting_power_encode(decoded);
+            expect(encoded2).to.deep.equal(encoded);
+        })
+    );
 });
--- src/rules/VotingPowerRule.ts
+++ src/rules/VotingPowerRule.ts
@@ -21,7 +21,7 @@ export class VotingPowerRule extends Rule {
         return Rule.Type.VotingPower;
     }
 
-    public async validate (voteorder: SendVoteorder, context: ValidationContext) {
+    public async validate(voteorder: SendVoteorder, context: ValidationContext) {
         this.validateRuleObject(this);
         const delegatorAccount = await context.getAccountInfo(context.getDelegatorUsername());
         if (!delegatorAccount) throw new Error("Delegator account info is undefined");
@@ -29,45 +29,74 @@ export class VotingPowerRule extends Rule {
         if (this.mode == VotingPowerRule.Mode.EQUAL) {
             if (delegatorAccount.voting_power !== this.value)
                 throw new ValidationException(
-                    "Delegator voting power (" + this.votingpowerToPercent(delegatorAccount.voting_power) + ") does not equal "
-                     + this.votingpowerToPercent(this.value)
+                    "Delegator voting power (" +
+                        this.votingpowerToPercent(delegatorAccount.voting_power) +
+                        ") does not equal " +
+                        this.votingpowerToPercent(this.value)
                 );
-        }
-        else if (this.mode == VotingPowerRule.Mode.MORE_THAN) {
+        } else if (this.mode == VotingPowerRule.Mode.MORE_THAN) {
             if (delegatorAccount.voting_power <= this.value)
                 throw new ValidationException(
-                    "Delegator voting power (" + this.votingpowerToPercent(delegatorAccount.voting_power) + ") is not more than "
-                    + this.votingpowerToPercent(this.value)
+                    "Delegator voting power (" +
+                        this.votingpowerToPercent(delegatorAccount.voting_power) +
+                        ") is not more than " +
+                        this.votingpowerToPercent(this.value)
                 );
-        }
-        else if (this.mode == VotingPowerRule.Mode.LESS_THAN) {
+        } else if (this.mode == VotingPowerRule.Mode.LESS_THAN) {
             if (delegatorAccount.voting_power >= this.value)
                 throw new ValidationException(
-                    "Delegator voting power (" + this.votingpowerToPercent(delegatorAccount.voting_power) + ") is not less than "
-                    + this.votingpowerToPercent(this.value)
+                    "Delegator voting power (" +
+                        this.votingpowerToPercent(delegatorAccount.voting_power) +
+                        ") is not less than " +
+                        this.votingpowerToPercent(this.value)
                 );
-        }
-        else {
+        } else {
             throw new Error("Unknown mode of voting power rule: " + this.mode);
         }
     }
 
     public validateRuleObject(unprototypedObj: any) {
         ["value", "mode"].forEach(prop => {
-            if (!_.has(unprototypedObj, prop)) throw new ValidationException("VotingPowerRule: property " + prop + " is missing");
+            if (!_.has(unprototypedObj, prop))
+                throw new ValidationException("VotingPowerRule: property " + prop + " is missing");
         });
-        if (!_.includes([VotingPowerRule.Mode.MORE_THAN, VotingPowerRule.Mode.LESS_THAN, VotingPowerRule.Mode.EQUAL], unprototypedObj.mode))
+
+        if (
+            !_.includes(
+                [VotingPowerRule.Mode.MORE_THAN, VotingPowerRule.Mode.LESS_THAN, VotingPowerRule.Mode.EQUAL],
+                unprototypedObj.mode
+            )
+        )
             throw new ValidationException("VotingPowerRule: unknown mode " + unprototypedObj.mode);
+
+        if (unprototypedObj.value > 10000) {
+            throw new ValidationException(
+                `VotingPowerRule: voting power (${unprototypedObj.value}) cannot be greater than 100%`
+            );
+        }
+
+        if (unprototypedObj.value < 0) {
+            throw new ValidationException(
+                `VotingPowerRule: voting power (${unprototypedObj.value}) cannot be less than 0%`
+            );
+        }
     }
 
     public getDescription(): string {
         let out = "The delegator has ";
 
         switch (this.mode) {
-            case VotingPowerRule.Mode.MORE_THAN: out += "more than"; break;
-            case VotingPowerRule.Mode.LESS_THAN: out += "less than"; break;
-            case VotingPowerRule.Mode.EQUAL: out += "exactly"; break;
-            default: out += this.mode;
+            case VotingPowerRule.Mode.MORE_THAN:
+                out += "more than";
+                break;
+            case VotingPowerRule.Mode.LESS_THAN:
+                out += "less than";
+                break;
+            case VotingPowerRule.Mode.EQUAL:
+                out += "exactly";
+                break;
+            default:
+                out += this.mode;
         }
         out += " " + this.votingpowerToPercent(this.value) + " voting power";
 
@@ -83,6 +112,6 @@ export namespace VotingPowerRule {
     export enum Mode {
         MORE_THAN = "more_than",
         LESS_THAN = "less_than",
-        EQUAL = "equal"
+        EQUAL = "equal",
     }
 }
